e9735c2149d1f2bad89b62215067e8039cfeedc4
boltVT <38771362+boltVT@users.noreply.github.com>
2018-04-27 19:46:58 -0400

    Add files via upload

---
+++ server.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+
+// initializing variables
+$username = "";
+$email    = "";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect('localhost', 'finaltest', 'kekek', 'forum_database');
+
+// LOGIN USER
+if (isset($_POST['btnSubmit'])) {
+  $username = mysqli_real_escape_string($db, $_POST['txtUsername']);
+  $password = mysqli_real_escape_string($db, $_POST['txtPassword']);
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['username'] = $username;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: index.php');
+  	}else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
+}
+?>
